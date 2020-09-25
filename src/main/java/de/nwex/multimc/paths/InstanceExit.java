40d8a000d64b5185563d8043caa518cd51424f2d
networkException <git@nwex.de>
2020-09-25 20:42:14 +0200

    InstanceExit: Reformat waypoint on exit of purevanilla instance

--- src/main/java/de/nwex/multimc/paths/InstanceExit.java
+++ src/main/java/de/nwex/multimc/paths/InstanceExit.java
@@ -5,7 +5,12 @@ import com.sun.net.httpserver.HttpHandler;
 import de.nwex.multimc.Main;
 import de.nwex.multimc.annotations.Path;
 import de.nwex.multimc.util.HandlerUtil;
+import de.nwex.multimc.util.UriUtil;
+import de.nwex.multimc.voxelmap.WaypointFormatter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.stream.Collectors;
 
 @Path("/exit")
 public class InstanceExit implements HttpHandler {
@@ -15,6 +20,19 @@ public class InstanceExit implements HttpHandler {
             HandlerUtil.send(exchange, 403, "Only local requests supported");
             exchange.close();
             return;
+        }
+
+        String name = UriUtil.splitQuery(exchange.getRequestURI()).getOrDefault("name", "undefined");
+        if(name.contains("purevanilla")) {
+            new Thread(() -> {
+                try {
+                    java.nio.file.Path filePath = java.nio.file.Path.of("/home/networkexception/common/minecraft/instances/Shared/mamiyaotaru/voxelmap/play.purevanilla.es.points");
+
+                    Files.write(filePath, WaypointFormatter.format(Files.lines(filePath).collect(Collectors.toList())), StandardCharsets.UTF_8);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
         }
 
         Main.getPresence().clear();
