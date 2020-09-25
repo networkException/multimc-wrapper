606066b1c95b6ac5c1462444724406e3b2636579
networkException <git@nwex.de>
2020-09-25 17:58:40 +0200

    UriUntil: Initial commit

---
+++ src/main/java/de/nwex/multimc/util/UriUtil.java
@@ -0,0 +1,34 @@
+package de.nwex.multimc.util;
+
+import com.google.common.base.Strings;
+import java.net.URI;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.AbstractMap.SimpleImmutableEntry;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class UriUtil {
+    public Map<String, List<String>> splitQuery(URI url) {
+        if (Strings.isNullOrEmpty(url.getQuery())) {
+            return Collections.emptyMap();
+        }
+        return Arrays.stream(url.getQuery().split("&"))
+            .map(this::splitQueryParameter)
+            .collect(Collectors.groupingBy(SimpleImmutableEntry::getKey, LinkedHashMap::new, Collectors.mapping(Map.Entry::getValue, Collectors.toList())));
+    }
+
+    public SimpleImmutableEntry<String, String> splitQueryParameter(String it) {
+        final int idx = it.indexOf("=");
+        final String key = idx > 0 ? it.substring(0, idx) : it;
+        final String value = idx > 0 && it.length() > idx + 1 ? it.substring(idx + 1) : null;
+        return new SimpleImmutableEntry<>(
+            URLDecoder.decode(key, StandardCharsets.UTF_8),
+            URLDecoder.decode(value != null ? value : "", StandardCharsets.UTF_8)
+        );
+    }
+}
