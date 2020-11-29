5c1f4f1a9c5ebdfa1cf3b39fb0ae678973c36314
networkException <git@nwex.de>
2020-11-29 18:56:20 +0100

    WaypointType: Removed plot in favor of warp

--- src/main/java/de/nwex/multimc/voxelmap/WaypointType.java
+++ src/main/java/de/nwex/multimc/voxelmap/WaypointType.java
@@ -5,11 +5,11 @@ import java.util.List;
 
 public enum WaypointType {
     BASE("Base", "red:0.35, green:0.52, blue:0.97", IconType.HOUSE),
-    STRUCTURE("Structure", "ed:0.44, green:1.0, blue:0.46", IconType.CAMERA),
+    STRUCTURE("Structure", "red:0.44, green:1.0, blue:0.46", IconType.CAMERA),
     MAP("Map", "red:0.98, green:0.29, blue:0.23", IconType.STAR),
     MEMORIAL("Memorial", "red:0.98, green:0.98, blue:0.34", IconType.WORLD),
     RESOURCES("Resources", "red:0.22, green:0.89, blue:0.92", IconType.PICKAXE),
-    PLOT("Plot", "red:0.34, green:1.0, blue:0.32", IconType.PICKAXE),
+    WARP("Warp", "red:0.34, green:1.0, blue:0.32", IconType.PICKAXE),
     EVENT("Event", "red:0.99, green:0.59, blue:0.31", IconType.STAR);
 
     private final String name;
