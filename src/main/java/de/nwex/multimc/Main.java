ec996a9588e69709a81f447afb3ada4868f5dc08
networkException <git@nwex.de>
2020-11-29 19:06:28 +0100

    Main: First argument now accepts a filepath to a waypoints file for formatting

--- src/main/java/de/nwex/multimc/Main.java
+++ src/main/java/de/nwex/multimc/Main.java
@@ -4,9 +4,13 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import de.nwex.multimc.annotations.Path;
 import de.nwex.multimc.discord.Presence;
+import de.nwex.multimc.voxelmap.WaypointFormatter;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.stream.Collectors;
 import org.reflections.Reflections;
 
 public class Main {
@@ -14,6 +18,14 @@ public class Main {
     private static Presence presence;
 
     public static void main(String[] args) throws IOException {
+        if(args.length == 1) {
+            java.nio.file.Path filePath = java.nio.file.Path.of(args[0]);
+
+            Files.write(filePath, WaypointFormatter.format(Files.lines(filePath).collect(Collectors.toList())), StandardCharsets.UTF_8);
+
+            return;
+        }
+
         HttpServer server = HttpServer.create(new InetSocketAddress(20240), 0);
 
         new Reflections("de.nwex.multimc.paths")
